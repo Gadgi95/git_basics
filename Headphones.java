e03cfbf2930ee7f7383aa8af9319e9a093bf3111
Murat Khanov <Gadgi9512@gmail.com>
2021-07-16 15:03:08 +0300

    to change Headphone, Main and BattaryLevel

--- Headphones.java
+++ Headphones.java
@@ -1,6 +1,45 @@
 public class Headphones {
-  String name;
-  int batteryLevel;
-  boolean noiseSuppression;
+  private String name;
+  private BattaryLevel batteryLevel;
+  private boolean noiseSuppression;
+  private boolean tryWirelles;
 
+  public Headphones(String name, int batteryLevel, boolean noiseSuppression, boolean tryWirelles) {
+    this.name = name;
+    BattaryLevel battaryLevel = new BattaryLevel();
+    this.noiseSuppression = noiseSuppression;
+    this.tryWirelles = tryWirelles;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getBatteryLevel() {
+    return batteryLevel;
+  }
+
+  public void setBatteryLevel(int batteryLevel) {
+    this.batteryLevel = batteryLevel;
+  }
+
+  public boolean isNoiseSuppression() {
+    return noiseSuppression;
+  }
+
+  public void setNoiseSuppression(boolean noiseSuppression) {
+    this.noiseSuppression = noiseSuppression;
+  }
+
+  public boolean isTryWirelles() {
+    return tryWirelles;
+  }
+
+  public void setTryWirelles(boolean tryWirelles) {
+    this.tryWirelles = tryWirelles;
+  }
 }
