ba5eb962e276804c780b8c523f94b41f71ef079c
Murat Khanov <Gadgi9512@gmail.com>
2021-07-19 00:32:35 +0300

    new Headphones

--- Main.java
+++ Main.java
@@ -2,6 +2,16 @@ public class Main {
 
   public static void main(String[] args) {
 
+    Headphones headphones = new Headphones(
+        "Sony",
+        "wf-1000x",
+        new Battery(true, 460, 100),
+        new Case(true, new Battery(true, 1250, 95)),
+        new TryWireless(true, true),
+        false,
+        new VoiceAssistance(true, "Alisa"));
+
+    System.out.println("Power on");
 
   }
 
