a4721696f56ead8d1960a1e4732cee7450843f28
Murat Khanov <Gadgi9512@gmail.com>
2021-07-19 00:16:31 +0300

    constructors, getters and setters

--- Battery.java
+++ Battery.java
@@ -1,9 +1,51 @@
 public class Battery {
-  private boolean battary;
+  private boolean battery;
   private int batteryVolume;
   private int batteryLevel;
-  private final int minBatteryLevel = 0;
-  private final int maxBatteryLevel = 100;
+
+  public Battery(boolean battary) {
+    this.battery = battary;
+  }
+
+  public Battery(boolean battary, int batteryVolume, int batteryLevel) {
+    this.battery = battary;
+    this.batteryVolume = batteryVolume;
+    this.batteryLevel = batteryLevel;
+  }
+
+  public boolean isBattery() {
+    return battery;
+  }
+
+  public void setBattery(boolean battery) {
+    this.battery = battery;
+  }
+
+  public int getBatteryVolume() {
+    return batteryVolume;
+  }
+
+  public void setBatteryVolume(int batteryVolume) {
+    this.batteryVolume = batteryVolume;
+  }
+
+  public int getBatteryLevel() {
+    return batteryLevel;
+  }
+
+  public void setBatteryLevel(int batteryLevel) {
+    this.batteryLevel = batteryLevel;
+  }
+
+  public int getMinBatteryLevel() {
+    int minBatteryLevel = 0;
+    return minBatteryLevel;
+  }
+
+  public int getMaxBatteryLevel() {
+    int maxBatteryLevel = 100;
+    return maxBatteryLevel;
+  }
 
   public void printInfoBattery() {
     //System.out.println("Battery level: "  + getBatteryLevel + "%");
--- Headphones.java
+++ Headphones.java
@@ -7,5 +7,71 @@ public class Headphones {
   private boolean microphone;
   private VoiceAssistance voiceAssistance;
 
+  public Headphones(String nameTM, String model, Battery battery, Case cases,
+      TryWireless tryWireless,
+      boolean microphone, VoiceAssistance voiceAssistance) {
+    this.nameTM = nameTM;
+    this.model = model;
+    this.battery = battery;
+    this.cases = cases;
+    this.tryWireless = tryWireless;
+    this.microphone = microphone;
+    this.voiceAssistance = voiceAssistance;
+  }
 
+  public String getNameTM() {
+    return nameTM;
+  }
+
+  public void setNameTM(String nameTM) {
+    this.nameTM = nameTM;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public void setModel(String model) {
+    this.model = model;
+  }
+
+  public Battery getBattery() {
+    return battery;
+  }
+
+  public void setBattery(Battery battery) {
+    this.battery = battery;
+  }
+
+  public Case getCases() {
+    return cases;
+  }
+
+  public void setCases(Case cases) {
+    this.cases = cases;
+  }
+
+  public TryWireless getTryWireless() {
+    return tryWireless;
+  }
+
+  public void setTryWireless(TryWireless tryWireless) {
+    this.tryWireless = tryWireless;
+  }
+
+  public boolean isMicrophone() {
+    return microphone;
+  }
+
+  public void setMicrophone(boolean microphone) {
+    this.microphone = microphone;
+  }
+
+  public VoiceAssistance getVoiceAssistance() {
+    return voiceAssistance;
+  }
+
+  public void setVoiceAssistance(VoiceAssistance voiceAssistance) {
+    this.voiceAssistance = voiceAssistance;
+  }
 }
